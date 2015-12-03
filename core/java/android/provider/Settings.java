dde88f8e8adeb2897d6b5ed60ffe0f38f1bf01ef
maxwen <max.weninger@gmail.com>
2015-12-03 05:33:05 -0500

    [1/2] base: unlink notification volume (Squashed)
    because of omni merging 6.0 into 5.1 just cherrypicking their commit wasnt gonna work, and had undesired consequences this squashs all the relevant parts needed for 6.0
    
    Squashed By: BeansTown106

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3244,6 +3244,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
